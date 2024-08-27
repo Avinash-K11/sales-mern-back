a8846608016833235fbc5dd2240ad13790f9bfa5
Avinash-K11 <avinashkunath2@gmail.com>
2024-08-27 10:25:07 +0530

    cors3

--- app.js
+++ app.js
@@ -13,17 +13,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //cors handling
-// CORS is enabled for the selected origins
-let corsOptions = {
-    origin: 'https://sales-mern.onrender.com'
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
 };
+
+app.use(cors(corsOpts));
 
 // Handle GET requests to / route
 app.use("*", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.use('/api',cors(corsOptions), api);
+app.use('/api', api);
 
 Promise.all([connectToDb()])
     .then( () => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
