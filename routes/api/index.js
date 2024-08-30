3f49ea65a08a01646fcfc3cc550dcd87643e97d9
Avinash-K11 <avinashkunath2@gmail.com>
2024-08-30 11:47:24 +0530

    second commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,14 +2,11 @@ import { Router } from 'express';
 
 import signUpUser from './signUpUser.js';
 import logIn from './logIn.js';
-import logout from './logout.js';
 
 const router = Router();
 
 router.post('/signup', signUpUser);
 
 router.post('/login', logIn);
-
-router.post('/logout', logout);
 
 export default router;
--- routes/api/logout.js
+++
@@ -1,6 +0,0 @@
-export default (req, res) => {
-    console.log('My log out page');
-    res.clearCookie('jwtoken', { path:'' });
-    res.status(200).send("User Logout");
-};
-  
